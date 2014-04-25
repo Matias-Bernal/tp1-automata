2c496a131dbb519c2b912147120fee8049207327
payomaty666@gmail.com <payomaty666@gmail.com@0333d9de-5625-bf0c-c16d-3fd07720bebe>
2014-04-25 17:03:34 +0000

    Arreglado algunos de los test del proyecto

--- trunk/src/Test.java
+++ trunk/src/Test.java
@@ -6,10 +6,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		try {
-			DFA dfaautomata = (DFA) FA.parse_form_file("src/test/dfa1.dot");
+			DFA dfaautomata = (DFA) FA.parse_form_file("src/test/dfa4.dot");
 			System.out.println(dfaautomata.to_dot());
-			NFALambda nfaLambdaAutomata = dfaautomata.toNFALambda();
-			System.out.println(nfaLambdaAutomata.to_dot());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
