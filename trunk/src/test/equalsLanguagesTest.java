674d59ce73e6f58ea1a660db0754aa7a67539b45
gcucatti@gmail.com <gcucatti@gmail.com@0333d9de-5625-bf0c-c16d-3fd07720bebe>
2014-06-12 03:20:13 +0000

    comentarios y test minimizacion

---
+++ trunk/src/test/equalsLanguagesTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+import utils.Triple;
+import automata.DFA;
+import automata.FA;
+import automata.State;
+
+/**
+ *
+ * @author gaston
+ */
+public class equalsLanguagesTest {
+    
+}
